dcc7e684a87cf6532b1dcaa1210a5e1f9925cf74
tattinio <tattinio@blackroomz.com>
2021-01-17 12:58:10 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# NAKIVO Backup & Replication Free Edition
+
+Integration für FreeNAS aus dem Standard "NAKIVO_Backup_Replication_v10.1.1.48504_Installer-FREE.sh" Installations script.
+
+## Support
+
+```
+support@nakivo.com
+```
---
+++ post_install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+PRODUCT='NAKIVO Backup & Replication Free Edition'
+#URL="https://d96i82q710b04.cloudfront.net/res/product/NAKIVO_Backup_Replication_v10.1.1.48504_Installer-TRIAL.sh"
+URL="https://cloud.blackroomz.com/s/srY48tEGiedCiot/download"
+SHA256="8e1b4343267bbebdcc126b2b29918dca43cab1f26e8b229b8cc62e884b08796e"
+
+PRODUCT_ROOT="/usr/local/nakivo"
+INSTALL="inst.sh"
+
+curl --fail --tlsv1.2 -o $INSTALL $URL
+if [ $? -ne 0 -o ! -e $INSTALL ]; then
+    echo "ERROR: Failed to get $PRODUCT installer"
+    rm $INSTALL >/dev/null 2>&1
+    exit 1
+fi
+
+CHECKSUM=`sha256 -q $INSTALL`
+if [ "$SHA256" != "$CHECKSUM" ]; then
+    echo "ERROR: Incorrect $PRODUCT installer checksum"
+    rm $INSTALL >/dev/null 2>&1
+    exit 2
+fi
+
+sh ./$INSTALL -f -y -i "$PRODUCT_ROOT" --eula-accept --extract 2>&1
+if [ $? -ne 0 ]; then
+    echo "ERROR: $PRODUCT install failed"
+    rm $INSTALL >/dev/null 2>&1
+    exit 3
+fi
+rm $INSTALL >/dev/null 2>&1
+
+#disable default HTTP ports redirect
+SVC_PATH="$PRODUCT_ROOT/director"
+awk 'BEGIN{A=0} /port="80/{A=1} {if (A==0) print $0} />/{A=0}' $SVC_PATH/tomcat/conf/server-linux.xml >$SVC_PATH/tomcat/conf/server-linux.xml_ 2>/dev/null
+mv $SVC_PATH/tomcat/conf/server-linux.xml_ $SVC_PATH/tomcat/conf/server-linux.xml >/dev/null 2>&1
+
+#enforce EULA
+PROFILE=`ls "$SVC_PATH/userdata/"*.profile 2>/dev/null | head -1`
+if [ "x$PROFILE" != "x" ]; then
+    sed -e 's@"system.licensing.eula.must.agree": false@"system.licensing.eula.must.agree": true@' "$PROFILE" >"${PROFILE}_" 2>/dev/null
+    mv "${PROFILE}_" "$PROFILE" >/dev/null 2>&1
+fi
+
+service nkv_dirsvc start >/dev/null 2>&1
